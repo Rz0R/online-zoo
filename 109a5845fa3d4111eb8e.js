534409744bc32966798209244bc81dbe3c8791d5
Rz0R <vpa05@mail.ru>
2023-03-25 20:57:39 +1000

    feat: deploy web site

---
+++ 109a5845fa3d4111eb8e.js
@@ -0,0 +1,1 @@
+import{menuInit}from"./menu.js";import{amountInit}from"./amount.js";import"../scss/style.scss";menuInit(),amountInit();
---
+++ 92bbf33e265bb2a134f5.js
@@ -0,0 +1,1 @@
+import{menuInit}from"./menu.js";import{moveWatchBtnInit}from"./moveWatchButton.js";import petsSliderInit from"./pets-slider/index.js";import testimonialsSliderInit from"./testimonials-slider/index.js";import"../scss/style.scss";menuInit(),moveWatchBtnInit(),petsSliderInit(),testimonialsSliderInit();
